4ffe5ce0c7a7c515eaa9a4efa8bd3c5c0dee00c1
TimaFrolov <tima1911@yandex.ru>
2026-09-11 11:13:39 +0300

    fixup! Add files to run with nix+uv

--- shell.nix
+++ shell.nix
@@ -1,8 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 pkgs.mkShell {
-    packages = with pkgs; [
-        pkg-config
-        libmysqlclient
-        uv
-    ];
+  packages = with pkgs; [
+    pkg-config
+    libmysqlclient
+    uv
+  ];
+
+  shellHook = "uv pip install 'setuptools<82.0.0' ";
 }
